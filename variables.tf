e79c301f5d389c0cad72e02bc85db8dd596f4c30
tgamvidushi <vidhushibansal.ext@globeandmail.com>
2023-11-22 15:25:15 +0530

    Added files

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "vpc_cidr" {
  description = "CIDR Block of the VPC"
- type = "string"   
+ type = string   
 }
 
 variable "subnet_cidr" {
  description = "CIDR block of the subnet"
- type = "string"   
+ type = string   
 }
